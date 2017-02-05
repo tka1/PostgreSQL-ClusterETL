1d2a4de665650b941e64837c02ffae52fee12e89
tka1 <tomi.j.kallio@kolumbus.fi>
2017-02-05 19:33:37 +0200

    rbn MONTHLY QUERY

--- rbn_monthly_avg.sql
+++ rbn_monthly_avg.sql
@@ -1,4 +1,4 @@
-﻿SELECT  avg(db),to_char(date,'YYYY-MM-DD'),band,tx_mode
+﻿SELECT  count(distinct callsign) de_call_count,count(*)as spot_count,avg(db),to_char(date,'YYYY-MM-DD') as date,band,tx_mode
   FROM cluster.rbn
-  group by to_char(date,'YYYY-MM-DD'),band,tx_mode
+  group by to_char(date,'YYYY-MM-DD'),band,tx_mode,callsign
   --order by to_char(date,'YYYY-MM-DD'),band,tx_mode
